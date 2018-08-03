e958f78a1eac6709420f02a642d5a3866e6c7dd8
guo <guoxian@evergrande.cn>
2018-08-03 14:25:36 +0800

    add

---
+++ fluent_py/ch10_seq_hack/protocols.py
@@ -0,0 +1,8 @@
+'''
+In  the  context  of  object-oriented  programming,  a  protocol  is  an  informal  interface,
+defined only in documentation and not in code. For example, the sequence protocol in
+Python entails just the __len__ and __getitem__ methods. Any class Spam that imple‐
+ments those methods with the standard signature and semantics can be used anywhere
+a sequence is expected. 
+
+'''
